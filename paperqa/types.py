c47060e36ed4999fcf95130e842f825c42a43434
Kiran Vijapure <kiran.vijapure@oneconvergence.com>
2024-06-14 07:07:09 +0000

    Added summarized_text and generated_questions as class variables to the class Text.

--- paperqa/types.py
+++ paperqa/types.py
@@ -57,6 +57,8 @@ class Text(BaseModel):
     doc_source: Optional[str] = None
     follow_on_question: Optional[bool] = None
     gen_question: Optional[str] = None
+    summarized_text: Optional[str] = None
+    generated_questions: Optional[str] = None
 
 class Faq_Text(BaseModel):
     question: str
